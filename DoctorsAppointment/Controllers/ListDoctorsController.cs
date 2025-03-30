aa710d041a679e564b8108acf21b23ed2b072cd6
fouadabujasser <fouad.abujasser@gmail.com>
2025-03-30 23:06:38 +0200

    Add project files.

---
+++ DoctorsAppointment/Controllers/ListDoctorsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DoctorsAppointment.Controllers
+{
+    public class ListDoctorsController : Controller
+    {
+        public IActionResult RetriveDoctors()
+        {
+            string msg1 = "List Doctors Page";
+            string msg2 = "Welcome to this Page";
+            int value = 150;
+
+            var msgs = new {msg1, msg2, value};
+            return View(model: msgs);
+        }
+    }
+}
