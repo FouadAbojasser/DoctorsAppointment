9bbb7ea3b2f0259d344e0662aaba018346e68492
fouadabujasser <fouad.abujasser@gmail.com>
2025-04-01 05:19:36 +0200

    Add Doctors List Page

---
+++ DoctorsAppointment/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DoctorsAppointment.Models;
+using Microsoft.EntityFrameworkCore;
+namespace DoctorsAppointment.Data
+
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=DoctorsAppointment;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+    }
+}
