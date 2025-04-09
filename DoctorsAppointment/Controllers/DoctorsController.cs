ac64ba42899d6476588671999fb47e69a473c644
fouadabujasser <fouad.abujasser@gmail.com>
2025-04-10 00:53:01 +0200

    Add Filter to Doctor List Page

--- DoctorsAppointment/Controllers/DoctorsController.cs
+++ DoctorsAppointment/Controllers/DoctorsController.cs
@@ -1,5 +1,6 @@
 ﻿using AspNetCoreGeneratedDocument;
 using DoctorsAppointment.Data;
+using DoctorsAppointment.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,11 +10,43 @@ namespace DoctorsAppointment.Controllers
     public class DoctorsController : Controller
     {
         private readonly ApplicationDbContext _context = new();
-        public IActionResult ListDoctors()
+        public IActionResult DoctorsList(string doctor_name, int specialization_id, string gender_name, string location)
         {
-            var doctors = _context.Doctors;
+           
+            IQueryable<Doctor> doctors = _context.Doctors.Include(d => d.Specialities);
+
+           
+            var distinctSpecialities = _context.Specialities.Distinct().OrderBy(e=>e.Title);
+            ViewData["specialities"] = distinctSpecialities.ToList();
+
+           
+            if (!string.IsNullOrEmpty(doctor_name))
+            {
+                doctors = doctors.Where(d => d.Name.Contains(doctor_name));
+                ViewBag.DoctorName = doctor_name;
+            }
+
+            if (specialization_id > 0 && specialization_id <= distinctSpecialities.Count())
+            {
+                doctors = doctors.Where(d => d.Specialities.Any(s => s.Id == specialization_id));
+                ViewBag.SpecializationId = specialization_id;
+            }
+
+            if (!string.IsNullOrEmpty(gender_name))
+            {
+                doctors = doctors.Where(d => d.Gender == gender_name);
+                ViewBag.Gender = gender_name;
+            }
+
+            if (!string.IsNullOrEmpty(location))
+            {
+                doctors = doctors.Where(d => d.Address.Contains(location));
+                ViewBag.Location = location;
+            }
+
             return View(doctors.ToList());
         }
+
 
         public IActionResult DoctorDetails(int id)
         {
